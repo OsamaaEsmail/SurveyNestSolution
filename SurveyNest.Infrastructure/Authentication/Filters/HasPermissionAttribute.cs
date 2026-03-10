707d6a079e7776d6739e20e93b39f89fde55ff84
Dev Osama Esmail <devosama291@gmail.com>
2026-03-11 01:28:05 +0200

    feat(auth): implement permission-based authorization with dynamic policies

---
+++ SurveyNest.Infrastructure/Authentication/Filters/HasPermissionAttribute.cs
@@ -0,0 +1,11 @@
+﻿
+
+using Microsoft.AspNetCore.Authorization;
+
+namespace SurveyNest.Infrastructure.Authentication.Filters;
+
+public class HasPermissionAttribute(string permission) : AuthorizeAttribute(permission)
+{
+
+
+}
---
+++ SurveyNest.Infrastructure/Authentication/Filters/PermissionRequirement.cs
@@ -0,0 +1,10 @@
+﻿
+
+using Microsoft.AspNetCore.Authorization;
+
+namespace SurveyNest.Infrastructure.Authentication.Filters;
+
+public class PermissionRequirement(string permission) : IAuthorizationRequirement
+{
+    public string Permission { get; } = permission;
+}
